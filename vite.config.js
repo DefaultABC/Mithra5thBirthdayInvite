680ca0cba8014cec2fdf001240acc3baee2257f4
Nivedha Dhakshnamurthy <82837138+nivedhadhaksh@users.noreply.github.com>
2026-03-09 08:09:39 +0000

    manifest

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Mithra5thBirthdays/',   // ← use repo name for project page
   plugins: [react()],
-});
+  base: '/Mithra5thBirthdays/'
+})
